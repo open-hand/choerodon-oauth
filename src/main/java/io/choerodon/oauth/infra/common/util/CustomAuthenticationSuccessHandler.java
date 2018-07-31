34b6f082ff01a02ef9f2f69bb8acc6769a79d728
Fan <fan@choerodon.io>
2018-07-31 19:27:58 +0800

    [FIX] fix https redirect http

--- src/main/java/io/choerodon/oauth/infra/common/util/CustomAuthenticationSuccessHandler.java
+++ src/main/java/io/choerodon/oauth/infra/common/util/CustomAuthenticationSuccessHandler.java
@@ -1,16 +1,21 @@
 package io.choerodon.oauth.infra.common.util;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.Date;
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.DefaultSavedRequest;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import io.choerodon.oauth.core.password.record.LoginRecord;
@@ -35,24 +40,51 @@ public class CustomAuthenticationSuccessHandler extends
     @Value("${choerodon.default.redirect.url:/}")
     private String defaultUrl;
 
+    @Value("${choerodon.oauth.login.ssl:false}")
+    private boolean useSSL;
+
     @PostConstruct
-    private void init (){
+    private void init() {
         this.setDefaultTargetUrl(defaultUrl);
     }
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
-                                        HttpServletResponse httpServletResponse,
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
                                         Authentication authentication)
             throws IOException, ServletException {
 
-        String username = httpServletRequest.getParameter("username");
+        String username = request.getParameter("username");
         UserDO userDO = userService.findUser(username);
         userDO.setLastLoginAt(new Date());
         userDO.setPasswordAttempt(0);
         userService.updateByPrimaryKeySelective(userDO);
         loginRecord.loginSuccess(userDO.getId());
-        super.onAuthenticationSuccess(httpServletRequest, httpServletResponse, authentication);
 
+        DefaultSavedRequest saveRequest = null;
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            saveRequest = (DefaultSavedRequest) session.getAttribute("SPRING_SECURITY_SAVED_REQUEST");
+            System.out.println("session is not null !!");
+        }
+
+        SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
+        try {
+            if(saveRequest == null) {
+                super.onAuthenticationSuccess(request, response, authentication);
+            }
+            if(useSSL) {
+                Field schemeField = DefaultSavedRequest.class.getDeclaredField("scheme");
+                schemeField.setAccessible(true);
+                schemeField.set(saveRequest, "https");
+                Field portStringField = DefaultSavedRequest.class.getDeclaredField("serverPort");
+                portStringField.setAccessible(true);
+                portStringField.set(saveRequest, 443);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        super.onAuthenticationSuccess(request, response, authentication);
     }
 }
