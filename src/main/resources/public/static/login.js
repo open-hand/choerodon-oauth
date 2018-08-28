bfee23087c725197e0488afd0b1c95def99fbb03
陈彬杰 <binjie.chen@hand-china.com>
2018-08-28 14:04:51 +0800

    [FIX] 修复点击按钮不发送请求

--- src/main/resources/public/static/login.js
+++ src/main/resources/public/static/login.js
@@ -10,7 +10,6 @@ class LoginButton extends window.React.Component{
     }
     handleButtonClickTest = (e) => {
         this.setState({ loading: true })
-
         const promise = new Promise((resolve, reject) => {
             setTimeout(resolve, 2000)
         });
@@ -18,11 +17,53 @@ class LoginButton extends window.React.Component{
         promise.then(() => {
             this.setState({ loading: false })
         })
+        $("#usernameIsNullMsg").css('display', 'none');
+        $("#passwordIsNullMsg").css('display', 'none');
+        $("#usernameOrPasswordNotFoundMsg").html("");
+        var username = $.trim($("#username").val());
+        var password = $.trim($("#password").val());
+        if (username == '') {
+            $("#usernameIsNullMsg").css('display', 'block');
+            return;
+        }
+        if (password == '') {
+            $("#passwordIsNullMsg").css('display', 'block');
+            return;
+        }
+        $("#md5_password").val(this.encode(password));
+        $('.login-form').submit();
     };
+
+   encode = (password) => {
+        var output = "";
+        var chr1, chr2, chr3 = "";
+        var enc1, enc2, enc3, enc4 = "";
+        var i = 0;
+        do {
+            chr1 = password.charCodeAt(i++);
+            chr2 = password.charCodeAt(i++);
+            chr3 = password.charCodeAt(i++);
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+            output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2)
+                + keyStr.charAt(enc3) + keyStr.charAt(enc4);
+            chr1 = chr2 = chr3 = "";
+            enc1 = enc2 = enc3 = enc4 = "";
+        } while (i < password.length);
+        return output;
+    }
+
 
     render() {
         return (
-            <Button htmlType="button" className="btn" onClick={this.handleButtonClickTest} loading={this.state.loading}><span>{this.state.loading ? '登陆中' : '登陆'}</span></Button>
+            <Button className="btn" onClick={this.handleButtonClickTest} loading={this.state.loading} htmlStyle="padding-top:4px"><span>{this.state.loading ? '登陆中' : '登陆'}</span></Button>
         )
     }
 }
@@ -54,16 +95,6 @@ class UsernameInupt extends window.React.Component{
             <Input autoFocus autoComplete="off" label="登陆账号" name="username" id="username" onChange={e => this.onValueChange(e)} placeholder="登录名/邮箱" value={this.state.currentUsername}/>
         )
     }
-}
-
-function getParam(paramName) {
-    let paramValue = "";
-    let isFound = !1;
-    if (this.location.search.indexOf("?") == 0 && this.location.search.indexOf("=") > 1) {
-        let arrSource = unescape(this.location.search).substring(1, this.location.search.length).split("&"), i = 0;
-        while (i < arrSource.length && !isFound) arrSource[i].indexOf("=") > 0 && arrSource[i].split("=")[0].toLowerCase() == paramName.toLowerCase() && (paramValue = arrSource[i].split("=")[1], isFound = !0), i++
-    }
-    return paramValue == "" && (paramValue = null), paramValue
 }
 
 /**
