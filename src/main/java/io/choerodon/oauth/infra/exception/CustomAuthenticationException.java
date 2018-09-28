7e57ba0a74815201221b0c6633c865ce6378c26d
Fan <fan@choerodon.io>
2018-09-28 15:32:46 +0800

    [FIX] lock user 500 error

--- src/main/java/io/choerodon/oauth/infra/exception/CustomAuthenticationException.java
+++ src/main/java/io/choerodon/oauth/infra/exception/CustomAuthenticationException.java
@@ -6,19 +6,20 @@ import org.springframework.security.core.AuthenticationException;
  * @author dongfan117@gmail.com
  */
 public class CustomAuthenticationException extends AuthenticationException {
-    private final String value;
+    private final transient Object[] parameters;
 
-    public CustomAuthenticationException(String msg, Throwable t, String value) {
+    public CustomAuthenticationException(String msg, Throwable t, Object... parameters) {
         super(msg, t);
-        this.value = value;
+        this.parameters = parameters;
     }
 
-    public CustomAuthenticationException(String msg, String value) {
+    public CustomAuthenticationException(String msg, Object... parameters) {
         super(msg);
-        this.value = value;
+        this.parameters = parameters;
     }
 
-    public String getValue() {
-        return value;
+    public Object[] getParameters() {
+        return parameters;
     }
+
 }
