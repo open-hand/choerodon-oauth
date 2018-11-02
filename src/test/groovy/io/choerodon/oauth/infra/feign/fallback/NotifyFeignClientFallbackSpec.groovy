f2d0aea095ae000eb7d7c2499636794d5957ed68
superlee <superleader8@gmail.com>
2018-11-02 15:00:37 +0800

    [IMP] 删除无用代码

--- src/test/groovy/io/choerodon/oauth/infra/feign/fallback/NotifyFeignClientFallbackSpec.groovy
+++ src/test/groovy/io/choerodon/oauth/infra/feign/fallback/NotifyFeignClientFallbackSpec.groovy
@@ -1,9 +1,8 @@
 package io.choerodon.oauth.infra.feign.fallback
 
 import io.choerodon.core.exception.CommonException
+import io.choerodon.core.notify.NoticeSendDTO
 import io.choerodon.oauth.IntegrationTestConfiguration
-import io.choerodon.oauth.api.dto.EmailSendDTO
-import io.choerodon.oauth.api.dto.WsSendDTO
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
@@ -20,15 +19,7 @@ class NotifyFeignClientFallbackSpec extends Specification {
 
     def "PostEmail"() {
         when: "方法调用"
-        notifyFeignClientFallback.postNotice(new EmailSendDTO())
-        then: "异常抛出"
-        def e = thrown(CommonException)
-        e.message == "notify.error"
-    }
-
-    def "PostPm"() {
-        when: "方法调用"
-        notifyFeignClientFallback.postPm(new WsSendDTO())
+        notifyFeignClientFallback.postNotice(new NoticeSendDTO())
         then: "异常抛出"
         def e = thrown(CommonException)
         e.message == "notify.error"
