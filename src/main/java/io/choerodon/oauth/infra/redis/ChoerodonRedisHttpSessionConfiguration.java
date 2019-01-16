0834b2e9e190dc91fbb90e35eafe361b57f48932
flyleft <jcalaz@163.com>
2019-01-16 10:05:04 +0800

    [IMP] 添加了RequestContextFilter，用以修复OAuth2ClientContext存入redis之后反序列化问题

--- src/main/java/io/choerodon/oauth/infra/redis/ChoerodonRedisHttpSessionConfiguration.java
+++ src/main/java/io/choerodon/oauth/infra/redis/ChoerodonRedisHttpSessionConfiguration.java
@@ -1,14 +1,10 @@
 package io.choerodon.oauth.infra.redis;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.concurrent.Executor;
-
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.EmbeddedValueResolverAware;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +13,6 @@ import org.springframework.context.annotation.ImportAware;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -29,41 +24,47 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.config.annotation.web.http.SpringHttpSessionConfiguration;
 import org.springframework.session.data.redis.RedisFlushMode;
 import org.springframework.session.data.redis.RedisOperationsSessionRepository;
-import org.springframework.session.data.redis.config.ConfigureNotifyKeyspaceEventsAction;
-import org.springframework.session.data.redis.config.ConfigureRedisAction;
+import org.springframework.session.web.http.SessionRepositoryFilter;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 import org.springframework.util.StringValueResolver;
+import org.springframework.web.filter.RequestContextFilter;
 
-@ConditionalOnProperty(name = "choerodon.redisHttpSession.enabled",havingValue = "true", matchIfMissing = true)
+import java.util.Arrays;
+import java.util.Map;
+
+@ConditionalOnProperty(name = "choerodon.redisHttpSession.enabled", havingValue = "true", matchIfMissing = true)
 @Configuration
 @EnableScheduling
 public class ChoerodonRedisHttpSessionConfiguration extends SpringHttpSessionConfiguration implements EmbeddedValueResolverAware, ImportAware {
     private Integer maxInactiveIntervalInSeconds = 1800;
-    private ConfigureRedisAction configureRedisAction = new ConfigureNotifyKeyspaceEventsAction();
     private String redisNamespace = "";
     private RedisFlushMode redisFlushMode;
-    private RedisSerializer<Object> defaultRedisSerializer;
-    private Executor redisTaskExecutor;
-    private Executor redisSubscriptionExecutor;
     private StringValueResolver embeddedValueResolver;
+    private RedisSerializer<Object> defaultRedisSerializer;
 
     public ChoerodonRedisHttpSessionConfiguration() {
         this.redisFlushMode = RedisFlushMode.ON_SAVE;
     }
 
     @Bean
+    @ConditionalOnMissingBean(RequestContextFilter.class)
+    public RequestContextFilter requestContextFilter() {
+        return new RequestContextFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean requestContextFilterChainRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean(requestContextFilter());
+        registration.setOrder(SessionRepositoryFilter.DEFAULT_ORDER + 1);
+        registration.setName("requestContextFilter");
+        return registration;
+    }
+
+    @Bean
     public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory, RedisOperationsSessionRepository messageListener) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        if (this.redisTaskExecutor != null) {
-            container.setTaskExecutor(this.redisTaskExecutor);
-        }
-
-        if (this.redisSubscriptionExecutor != null) {
-            container.setSubscriptionExecutor(this.redisSubscriptionExecutor);
-        }
-
         container.addMessageListener(messageListener, Arrays.asList(new PatternTopic("__keyevent@*:del"), new PatternTopic("__keyevent@*:expired")));
         container.addMessageListener(messageListener, Arrays.asList(new PatternTopic(messageListener.getSessionCreatedChannelPrefix() + "*")));
         return container;
@@ -71,7 +72,7 @@ public class ChoerodonRedisHttpSessionConfiguration extends SpringHttpSessionCon
 
     @Bean
     public RedisTemplate<Object, Object> sessionRedisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<Object, Object> template = new RedisTemplate();
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
         if (this.defaultRedisSerializer != null) {
@@ -90,12 +91,10 @@ public class ChoerodonRedisHttpSessionConfiguration extends SpringHttpSessionCon
         if (this.defaultRedisSerializer != null) {
             sessionRepository.setDefaultSerializer(this.defaultRedisSerializer);
         }
-
-        String redisNamespace = this.getRedisNamespace();
-        if (StringUtils.hasText(redisNamespace)) {
-            sessionRepository.setRedisKeyNamespace(redisNamespace);
+        String tempRedisNamespace = this.getRedisNamespace();
+        if (StringUtils.hasText(tempRedisNamespace)) {
+            sessionRepository.setRedisKeyNamespace(tempRedisNamespace);
         }
-
         sessionRepository.setRedisFlushMode(this.redisFlushMode);
         return sessionRepository;
     }
@@ -120,45 +119,18 @@ public class ChoerodonRedisHttpSessionConfiguration extends SpringHttpSessionCon
     public void setImportMetadata(AnnotationMetadata importMetadata) {
         Map<String, Object> enableAttrMap = importMetadata.getAnnotationAttributes(EnableRedisHttpSession.class.getName());
         AnnotationAttributes enableAttrs = AnnotationAttributes.fromMap(enableAttrMap);
-        this.maxInactiveIntervalInSeconds = (Integer)enableAttrs.getNumber("maxInactiveIntervalInSeconds");
+        this.maxInactiveIntervalInSeconds = enableAttrs.getNumber("maxInactiveIntervalInSeconds");
         String redisNamespaceValue = enableAttrs.getString("redisNamespace");
         if (StringUtils.hasText(redisNamespaceValue)) {
             this.redisNamespace = this.embeddedValueResolver.resolveStringValue(redisNamespaceValue);
         }
 
-        this.redisFlushMode = (RedisFlushMode)enableAttrs.getEnum("redisFlushMode");
+        this.redisFlushMode = enableAttrs.getEnum("redisFlushMode");
     }
 
     @Autowired(
             required = false
     )
-    public void setConfigureRedisAction(ConfigureRedisAction configureRedisAction) {
-        this.configureRedisAction = configureRedisAction;
-    }
-
-    @Autowired(
-            required = false
-    )
-    @Qualifier("springSessionDefaultRedisSerializer")
-    public void setDefaultRedisSerializer(RedisSerializer<Object> defaultRedisSerializer) {
-        this.defaultRedisSerializer = defaultRedisSerializer;
-    }
-
-    @Autowired(
-            required = false
-    )
-    @Qualifier("springSessionRedisTaskExecutor")
-    public void setRedisTaskExecutor(Executor redisTaskExecutor) {
-        this.redisTaskExecutor = redisTaskExecutor;
-    }
-
-    @Autowired(
-            required = false
-    )
-    @Qualifier("springSessionRedisSubscriptionExecutor")
-    public void setRedisSubscriptionExecutor(Executor redisSubscriptionExecutor) {
-        this.redisSubscriptionExecutor = redisSubscriptionExecutor;
-    }
 
     public void setEmbeddedValueResolver(StringValueResolver resolver) {
         this.embeddedValueResolver = resolver;
@@ -169,31 +141,4 @@ public class ChoerodonRedisHttpSessionConfiguration extends SpringHttpSessionCon
         return new PropertySourcesPlaceholderConfigurer();
     }
 
-    static class EnableRedisKeyspaceNotificationsInitializer implements InitializingBean {
-        private final RedisConnectionFactory connectionFactory;
-        private ConfigureRedisAction configure;
-
-        EnableRedisKeyspaceNotificationsInitializer(RedisConnectionFactory connectionFactory, ConfigureRedisAction configure) {
-            this.connectionFactory = connectionFactory;
-            this.configure = configure;
-        }
-
-        public void afterPropertiesSet() throws Exception {
-            if (this.configure != ConfigureRedisAction.NO_OP) {
-                RedisConnection connection = this.connectionFactory.getConnection();
-
-                try {
-                    this.configure.configure(connection);
-                } finally {
-                    try {
-                        connection.close();
-                    } catch (Exception var8) {
-                        LogFactory.getLog(this.getClass()).error("Error closing RedisConnection", var8);
-                    }
-
-                }
-
-            }
-        }
-    }
 }
