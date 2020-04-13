992c15cb37b49882ccdb7861b73faf5bb249c9ea
scp <changping.shi@hand-china.com>
2020-04-13 10:31:17 +0800

    [ADD] 服务初始化

---
+++ src/main/java/io/choerodon/oauth/OauthApplication.java
@@ -0,0 +1,17 @@
+package io.choerodon.oauth;
+
+import org.hzero.autoconfigure.oauth.EnableHZeroOauth;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableHZeroOauth
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthApplication.class, args);
+    }
+
+}
