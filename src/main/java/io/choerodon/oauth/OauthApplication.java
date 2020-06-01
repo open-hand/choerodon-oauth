e2c4a4fe3bbbe885eeacb6b1026babfd0e9e33e6
王灏 <2507172753@qq.com>
2020-06-02 00:19:27 +0800

    [imp]添加grafana授权校验逻辑

--- src/main/java/io/choerodon/oauth/OauthApplication.java
+++ src/main/java/io/choerodon/oauth/OauthApplication.java
@@ -4,9 +4,11 @@ import org.hzero.autoconfigure.oauth.EnableHZeroOauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableHZeroOauth
 @EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class OauthApplication {
 
