83cccb7616315928950275540c50a448442a8f8d
18986 <changping.shi@hand-chian.com>
2021-03-01 18:57:00 +0800

    [IMP] 修改配置信息

--- src/main/java/io/choerodon/oauth/security/config/C7nSecurityConfig.java
+++ src/main/java/io/choerodon/oauth/security/config/C7nSecurityConfig.java
@@ -1,5 +1,6 @@
 package io.choerodon.oauth.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.web.http.CookieSerializer;
@@ -8,17 +9,18 @@ import org.springframework.session.web.http.DefaultCookieSerializer;
 /**
  * @author scp
  * @date 2020/9/17
- * @description
- * 替换TokenStore 为c7n自定义的 C7nCustomRedisTokenStore
+ * @description 替换TokenStore 为c7n自定义的 C7nCustomRedisTokenStore
  */
 @Configuration
 public class C7nSecurityConfig {
+    @Autowired
+    private CookieConfiguration cookieConfiguration;
 
     @Bean
     public CookieSerializer cookieSerializer() {
         DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
-        cookieSerializer.setSameSite("None");
-        cookieSerializer.setUseSecureCookie(true);
+        cookieSerializer.setSameSite(cookieConfiguration.getSameSite());
+        cookieSerializer.setUseSecureCookie(cookieConfiguration.isSecure());
         return cookieSerializer;
     }
 
