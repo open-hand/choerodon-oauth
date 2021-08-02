2eafffef3bee54de04654575234274f18f6b5778
wangxiang <xiangwang04@hand-china.com>
2021-08-02 16:58:31 +0800

    [FIX] 用户有权限的租户没有被禁用，则用户能登录

---
+++ src/main/java/io/choerodon/oauth/infra/mapper/TenantMapper.java
@@ -0,0 +1,16 @@
+package io.choerodon.oauth.infra.mapper;
+
+
+import java.util.List;
+
+import io.choerodon.mybatis.common.BaseMapper;
+import io.choerodon.oauth.infra.dto.TenantE;
+
+
+/**
+ * @author wuguokai
+ */
+public interface TenantMapper extends BaseMapper<TenantE> {
+
+
+}
