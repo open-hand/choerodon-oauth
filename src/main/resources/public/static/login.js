b7f1c3c8724723aecbdda5e36b0facdcef56f9de
jamin <245139263@qq.com>
2021-09-06 22:25:54 +0800

    [fix]加密bug

--- src/main/resources/public/static/login.js
+++ src/main/resources/public/static/login.js
@@ -161,7 +161,9 @@ class Content extends window.React.Component {
   }
   submitBtnClick() {
     let input = document.getElementById("pswinput")
-    input.value= this.encryptMd5(input.value)
+    let encrypt = new JSEncrypt()
+    encrypt.setPublicKey(this.state.publicKey); // 加密
+    input.value = encrypt.encrypt(input.value)
     if (!this.state.captchaKey && this.state.activeKey === "2") {
       if (
         this.state.phoneValidateSuccess &&
@@ -177,43 +179,10 @@ class Content extends window.React.Component {
   }
   formSubmit() {
     if (this.state.activeKey === "1") {
-      let input = document.getElementById("pswinput")
-      input.setAttribute('value',this.encryptMd5(input.value));
+      let input = document.getElementById("pswinput");
+      input.setAttribute("value", input.value);
     }
     $('#myForm').submit()
-  }
-  encryptMd5 = (password) => {
-    var output = "";
-    var chr1,
-        chr2,
-        chr3 = "";
-    var enc1,
-        enc2,
-        enc3,
-        enc4 = "";
-    var i = 0;
-
-    do {
-      chr1 = password.charCodeAt(i++);
-      chr2 = password.charCodeAt(i++);
-      chr3 = password.charCodeAt(i++);
-      enc1 = chr1 >> 2;
-      enc2 = (chr1 & 3) << 4 | chr2 >> 4;
-      enc3 = (chr2 & 15) << 2 | chr3 >> 6;
-      enc4 = chr3 & 63;
-
-      if (isNaN(chr2)) {
-        enc3 = enc4 = 64;
-      } else if (isNaN(chr3)) {
-        enc4 = 64;
-      }
-
-      output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + keyStr.charAt(enc3) + keyStr.charAt(enc4);
-      chr1 = chr2 = chr3 = "";
-      enc1 = enc2 = enc3 = enc4 = "";
-    } while (i < password.length);
-
-    return output;
   }
   phoneLabel = (
     <span>
