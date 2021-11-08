2aabb046776bc054311138b6126a51d77c858cb4
jamin <245139263@qq.com>
2021-11-08 11:41:58 +0800

    [fix]-登录样式

--- src/main/resources/public/static/login.js
+++ src/main/resources/public/static/login.js
@@ -529,7 +529,9 @@ class Content extends window.React.Component {
               </Tabs>
             </div>
           </div>
+          <div className='c-btn-img'>
           <img src="../static/1.svg" alt="" className="btm-img" />
+          </div>
           <div className="btm-desc">
             <p>
               © Copyright Hand China Co.,Ltd. All Rights Reserved
