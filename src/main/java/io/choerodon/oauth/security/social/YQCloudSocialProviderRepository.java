2545243e9d137308eecbf7e679667cccd38d5be4
18986 <changping.shi@hand-chian.com>
2022-04-07 15:26:12 +0800

    [IMP] 添加打印日志

--- src/main/java/io/choerodon/oauth/security/social/YQCloudSocialProviderRepository.java
+++ src/main/java/io/choerodon/oauth/security/social/YQCloudSocialProviderRepository.java
@@ -8,22 +8,29 @@ import org.hzero.boot.oauth.domain.entity.BaseOpenApp;
 import org.hzero.boot.oauth.domain.repository.BaseOpenAppRepository;
 import org.hzero.oauth.security.social.CustomSocialProviderRepository;
 import org.hzero.starter.social.core.provider.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+
 @Primary
 @Component
 public class YQCloudSocialProviderRepository extends CustomSocialProviderRepository {
+    private static final Logger LOGGER = LoggerFactory.getLogger(YQCloudSocialProviderRepository.class);
+
     @Autowired
     private BaseOpenAppRepository baseOpenAppRepository;
+
     @Override
     public List<Provider> getProvider(String providerId) {
+        LOGGER.info("================================providerId:{}", providerId);
         BaseOpenApp params = new BaseOpenApp();
         params.setType(providerId);
         List<BaseOpenApp> apps = this.baseOpenAppRepository.select(params);
         return CollectionUtils.isEmpty(apps) ? Collections.emptyList() : apps.stream().map((app) -> {
-            Provider provider = new Provider(providerId,"pc", app.getAppId(), app.getAppSecret(), null, app.getTenantId());
+            Provider provider = new Provider(providerId, "pc", app.getAppId(), app.getAppSecret(), null, app.getTenantId());
 //            provider.setAuthorizeUrl(app.getAuthUrl());
             return provider;
         }).collect(Collectors.toList());
